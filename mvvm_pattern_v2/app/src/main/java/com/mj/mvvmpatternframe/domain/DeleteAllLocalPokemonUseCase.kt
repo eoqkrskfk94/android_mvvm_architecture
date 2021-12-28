9b0fbbb1ccbe2261cd09dc08596a0d8d6a517ea4
danielkim <37679062+eoqkrskfk94@users.noreply.github.com>
2021-12-28 23:22:10 +0900

    [Edit] delete usecase having 2 functions

--- mvvm_pattern_v2/app/src/main/java/com/mj/mvvmpatternframe/domain/DeleteAllLocalPokemonUseCase.kt
+++ mvvm_pattern_v2/app/src/main/java/com/mj/mvvmpatternframe/domain/DeleteAllLocalPokemonUseCase.kt
@@ -4,11 +4,13 @@ import com.mj.mvvmpatternframe.data.entity.PokemonEntity
 import com.mj.mvvmpatternframe.data.repository.PokemonRepository
 import javax.inject.Inject
 
-class DeleteLocalPokemonUseCase @Inject constructor(
+class DeleteAllLocalPokemonUseCase @Inject constructor(
     private val pokemonRepository: PokemonRepository
 ) {
 
-    suspend operator fun invoke(pokemonEntity: PokemonEntity) {
-        pokemonRepository.deletePokemonEntity(pokemonEntity)
+    suspend operator fun invoke() {
+        pokemonRepository.deleteAllPokemon()
     }
+
+
 }
